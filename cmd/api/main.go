dad87e1aa43e6b4c434581e1c951cb1ef2bcb626
kriengkai <kriengkai.s@ku.th>
2026-04-09 12:30:15 +0700

    fix: edit cors allow url

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -62,7 +62,7 @@ func main() {
 
 func corsMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Origin", "https://url-shorterner-theta.vercel.app/")
         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
